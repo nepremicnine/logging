2e435ed204f45619465de2c0ce66398eb3e901ae
potocnikvid <vidpotocnik0@gmail.com>
2025-01-07 20:18:06 +0100

    added prefix

--- src/main.py
+++ src/main.py
@@ -1,27 +1,39 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.routing import APIRoute
 from models import SearchLog, VisitedLog
 from auth_handler import verify_jwt_token, get_supabase_client
 from dotenv import load_dotenv
 import os
 import requests
 import pybreaker
-import re
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type, RetryError
 
 # Load environment variables
 load_dotenv()
 
+# Environment variables
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_GRAPHQL_URL = f"{SUPABASE_URL}/graphql/v1"
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+LOGGING_API_SERVER_PORT = os.getenv("LOGGING_API_SERVER_PORT", "8080")
+LOGGING_API_SERVER_MODE = os.getenv("LOGGING_API_SERVER_MODE", "development")
+LOGGING_API_PREFIX = f"/logging" if LOGGING_API_SERVER_MODE == "production" else ""
+
 app = FastAPI()
+
+# Add prefix to all routes dynamically
+def add_prefix_to_routes(app: FastAPI, prefix: str):
+    """Add a prefix to all API routes."""
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.path = f"{prefix}{route.path}"
 
 # Circuit Breaker Configuration
 breaker = pybreaker.CircuitBreaker(
-    fail_max=5,  
-    reset_timeout=30  
+    fail_max=5,  # Maximum number of failures before opening the circuit
+    reset_timeout=30  # Time in seconds before attempting to reset the circuit
 )
 
 # Retry Configuration
@@ -34,7 +46,6 @@ retry_strategy = retry(
     wait=wait_exponential(multiplier=1, min=2, max=6),  # Exponential backoff: 2s, 4s, 6s
     retry=retry_if_exception_type(requests.exceptions.RequestException)  # Retry only on network-related errors
 )
-
 
 # Insert a search log
 @app.post("/search_log")
@@ -62,7 +73,6 @@ async def insert_search_log(search_log: SearchLog):
         return {"Log inserted": response}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
 
 # Insert a visited log
 @app.post("/visited_log")
@@ -82,4 +92,6 @@ async def insert_visited_log(visited_log: VisitedLog):
         return {"Log inserted": response}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
+
+# Add prefix to all routes
+add_prefix_to_routes(app, LOGGING_API_PREFIX)
